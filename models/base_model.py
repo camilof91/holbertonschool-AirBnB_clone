c2dea576f5b8b4926a1c96bde3bf20c77b02a099
camilofetecua1 <camilofetecua2011@outlook.com>
2024-03-12 21:55:31 -0500

    added class comments

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,17 @@
 import uuid
 from datetime import datetime
+"""
+    Represents a sample class with attributes
+    for identification and timestamps.
+
+    Attributes:
+        id (uuid.UUID): Unique identifier generated using uuid.uuid4().
+        created_at (datetime.datetime):
+            Date and time when the instance is created.
+        updated_at (datetime.datetime):
+            Date and time of the last update of the instance.
+"""
+
 
 class BaseModel:
     def __init__(self):
@@ -11,20 +23,19 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-
     def __str__(self):
         """
         Returns the form in which the information should be printed
         """
         class_name = self.__class__.__name__
-        return f"[{class_name}] ({self.id}) {self.__dict__}"         
-    
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
+
     def save(self):
         """
         Save the date current
         """
         self.update_at = datetime.now()
-    
+
     def to_dict(self):
         """Convert the instance to a dictionary.
 
@@ -36,4 +47,3 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
