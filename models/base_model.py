6d0f8a8814f959ba2e8ce00cccf00f094a6517e0
JBMjese <6935@holbertonstudents.com>
2024-03-12 19:09:19 -0500

    dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -9,3 +9,16 @@ class BaseModel:
     
     def save(self):
         return
+    
+    def _to_dict(self):
+        """Convert the instance to a dictionary.
+
+    Returns:
+        dict: A dictionary containing all attributes of the instance.
+    """
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()  
+        
+        self.__dict__["__class__"] = self.__class__.__name__
+        
+        return self.__dict__
