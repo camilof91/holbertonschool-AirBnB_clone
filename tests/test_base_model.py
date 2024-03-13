27187599e8573685aab50322fa5565ffc6433018
camilofetecua1 <camilofetecua2011@outlook.com>
2024-03-12 22:00:57 -0500

    fix code style

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
 """
 Here we are testing the file base_model
 """
-import unittest
-from models.base_model import BaseModel
+
 
 class TestBaseModel(unittest.TestCase):
     def test_str(self):
         objeto = BaseModel()
         result = str(objeto)
-        self.assertEqual(result, f"[BaseModel] ({objeto.id}) {objeto.__dict__}")
+        self.assertEqual(
+            result, f"[BaseModel] ({objeto.id}) {objeto.__dict__}")
