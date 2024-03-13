9d65d82a51a1c1c7897c96f7a8c0fba61c1dc524
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-03-13 14:21:06 -0500

    test save and str

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+from time import sleep
+import copy
+"""
+Here we are testing the file base_model
+"""
+
+        
+class TestBaseModel(unittest.TestCase):
+    def test_save(self):
+        objeto = BaseModel()
+        initial_time = copy.deepcopy(objeto.updated_at)
+        sleep(1)
+        objeto.save()
+        time_after_save = objeto.updated_at
+        self.assertNotEqual(initial_time, time_after_save)
+        self.assertIsInstance(time_after_save, datetime)
+
+    def test_str(self):
+        objeto = BaseModel()
+        result = str(objeto)
+        self.assertEqual(
+            result, f"[BaseModel] ({objeto.id}) {objeto.__dict__}")
