571b7a1ecc06983f2a9e6073d08e3339c70480bc
JBMjese <6935@holbertonstudents.com>
2024-03-14 07:05:26 -0500

    console implementation

---
+++ console.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNB command interpreter class.
+
+    This class provides an interactive command interpreter for the HBNB program.
+    Available commands:
+        - quit: Exit the program.
+        - EOF: Exit the program (Ctrl + D).
+        - help: Display help about available commands.
+    """
+    prompt = "(hbnb)"
+    
+    def do_quit(self, arg):
+        """Exit the program"""
+        return True
+    
+    def do_EOF(self, arg):
+        """Exit the program when Ctrl + D is pressed"""
+        print("")
+        return True
+    
+    def do_help(self, arg):
+        """Show help"""    
+        if arg == "quit":
+            print("Quit command to exit the program")
+        else:        
+            cmd.Cmd.do_help(self, arg)
+        
+    def emptyline(self):
+        pass    
+    
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
