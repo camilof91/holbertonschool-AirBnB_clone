ece70637dd2ce9a0284c626f5dad80d25610adbc
camilofetecua1 <camilofetecua2011@outlook.com>
2024-03-15 14:07:18 -0500

    Write all those classes that inherit from BaseModel

---
+++ models/place.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines the Place class."""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """Represents a place.
+
+    Attributes:
+        city_id (str): The ID of the city associated with the place.
+        user_id (str): The ID of the user who owns the place.
+        name (str): The name of the place.
+        description (str): The description of the place.
+        number_rooms (int): The number of rooms in the place.
+        number_bathrooms (int): The number of bathrooms in the place.
+        max_guest (int): The maximum number of guests the place can accommodate.
+        price_by_night (int): The price per night for the place.
+        latitude (float): The latitude coordinate of the place.
+        longitude (float): The longitude coordinate of the place.
+        amenity_ids (list of str): The IDs of the amenities associated with the place.
+    """
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
--- models/review.py
+++ models/review.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Defines the Review class."""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Represents a review.
+
+    Attributes:
+        place_id (str): The ID of the place associated with the review.
+        user_id (str): The ID of the user who created the review.
+        text (str): The content of the review.
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
